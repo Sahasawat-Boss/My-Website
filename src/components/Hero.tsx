e85e7dbfad202bef782c0b7de33aa2d84dd7c553
Boss <sahasawat.rk@gmail.com>
2025-02-27 22:32:59 +0700

    Update GUI

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,7 +39,7 @@ function Hero() {
         <motion.section
             ref={ref}
             style={{ backgroundImage }}
-            className="relative flex justify-center items-center xs:h-[80vh] px-6 py-24 text-gray-200"
+            className="relative flex justify-center items-center h-[80vh] px-6 py-24 text-gray-200"
             initial={{ opacity: 0, y: 50 }}
             animate={isInView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.8, ease: "easeOut" }}
